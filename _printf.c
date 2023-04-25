9401f2e4891029e05676f1ec3bbe4c11b1977d77
Yassine Rakibi <yassine@arcraven.com>
2023-04-25 14:18:16 +0200

    task 0: _printf.c

--- _printf.c
+++ _printf.c
@@ -11,24 +11,24 @@
 
 int _printf(const char *format, ...)
 {
-	int i, c = 0;
+	int c = 0;
 	char *str, ch;
 	va_list ap;
 
 	if (format == NULL)
 		return (-1);
 	va_start(ap, format);
-	for (i = 0; format[i]; i++)
+	while (*format)
 	{
-		if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's'))
+		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
 		{
-			c += _putchar(format[i]);
-			if (format[i + 1] == 'c')
+			c += _putchar(*format);
+			if (*(format + 1) == 'c')
 			{
 				ch = va_arg(ap, int);
-				c += _putchar(ch), i++;
+				c += _putchar(ch);
 			}
-			else if (format[i + 1] == 's')
+			else if (*(format + 1) == 's')
 			{
 				str = va_arg(ap, char *);
 				if (str == NULL)
@@ -38,17 +38,16 @@ int _printf(const char *format, ...)
 					_putchar(*str);
 					str++;
 				}
-				c += strlen(str), i++;
+				c += strlen(str);
 			}
 		}
-		else if (format[i] == '\0')
+		else if (*format == '\0')
 		{
 			return (-1);
 		}
 		else
-		{
-			c += _putchar(format[i]);
-		}
+			c += _putchar(*format);
+		format++;
 	} va_end(ap);
 	return (c);
 }
