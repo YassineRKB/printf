d080f38281eb546816c909ebf7bab14310087970
Yassine Rakibi <yassine@arcraven.com>
2023-04-25 10:58:48 +0200

    task 0: created _printf.c

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+* _printf - function that produces output according to a format.
+* @foramt: const char
+* Return: char.
+**/
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
